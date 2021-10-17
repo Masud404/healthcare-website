3bb6c584464bee19a55af06abf76d1707fe394ff
Masud404 <masud215502@gmail.com>
2021-10-18 02:35:26 +0600

    first commit

---
+++ src/Component/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyCcirptgiAiNdsJUEsoDrdtQ64gVzTH6MM",
+
+    authDomain: "healthcare-related-websi-ce7dc.firebaseapp.com",
+
+    projectId: "healthcare-related-websi-ce7dc",
+
+    storageBucket: "healthcare-related-websi-ce7dc.appspot.com",
+
+    messagingSenderId: "11333436495",
+
+    appId: "1:11333436495:web:0de40fe8d062e3ea06d63a"
+
+};
+
+export default firebaseConfig;
